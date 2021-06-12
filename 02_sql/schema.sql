a36496ca65807979baacd0598ba6ec019cc66be4
sd70 <saeed.haddadian@gmail.com>
2021-06-12 12:24:54 +0430

    schema.sql created

---
+++ 02_sql/schema.sql
@@ -0,0 +1,69 @@
+START TRANSACTION;
+
+DROP TABLE IF EXISTS video_watch;
+DROP TABLE IF EXISTS channel_video;
+DROP TABLE IF EXISTS video;
+DROP TABLE IF EXISTS channel_subscription;
+DROP TABLE IF EXISTS channel;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+	userid SERIAL PRIMARY KEY,
+	username VARCHAR(30) UNIQUE NOT NULL,
+	password CHAR(34) NOT NULL, /* MD5-based crypt */
+	email VARCHAR(50) UNIQUE NOT NULL,
+	reg_date TIMESTAMP NOT NULL,
+	has_avatar BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE video (
+	video_id SERIAL PRIMARY KEY,
+	userid SERIAL NOT NULL, /* video owner */
+	name VARCHAR(50) NOT NULL,
+	filename VARCHAR(50) NOT NULL,
+	description TEXT NOT NULL,
+	duration INTEGER NOT NULL CHECK (duration > 0) /* unit: seconds */,
+	upload_date TIMESTAMP NOT NULL,
+
+	UNIQUE(userid, name),
+	FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE video_watch (
+	watch_id SERIAL PRIMARY KEY,
+	video_id SERIAL NOT NULL,
+	userid SERIAL NOT NULL,
+	watch_date TIMESTAMP NOT NULL,
+
+	FOREIGN KEY (video_id) REFERENCES video(video_id),
+	FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE channel (
+	channel_id SERIAL PRIMARY KEY,
+	userid SERIAL NOT NULL, /* channel owner */
+	name VARCHAR(50) NOT NULL,
+	description TEXT NOT NULL,
+	creation_date TIMESTAMP NOT NULL,
+
+	FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+CREATE TABLE channel_subscription (
+	channel_id SERIAL,
+	userid SERIAL,
+
+	PRIMARY KEY (channel_id, userid),
+	FOREIGN KEY (userid) REFERENCES users(userid),
+	FOREIGN KEY (channel_id) REFERENCES channel(channel_id)
+);
+
+CREATE TABLE channel_video (
+	video_id SERIAL PRIMARY KEY,
+	channel_id SERIAL NOT NULL,
+
+	FOREIGN KEY (video_id) REFERENCES video(video_id),
+	FOREIGN KEY (channel_id) REFERENCES channel(channel_id)
+);
+
+COMMIT;
