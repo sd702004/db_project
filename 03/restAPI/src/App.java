206bc4f9d93217e97dd4365f7681774a08a20beb
sd70 <saeed.haddadian@gmail.com>
2021-06-30 16:14:26 +0430

    main function updated

--- 03/restAPI/src/App.java
+++ 03/restAPI/src/App.java
@@ -1,10 +1,24 @@
 import cfg.ConfigLoader;
+import handler.DBHandler;
 
 public class App{
 	public static void main(String args[]){
+		// load config
 		ConfigLoader config = new ConfigLoader();
 
 		if (!config.loadConfigFile())
+			System.exit(1);
+
+		// connect to database
+		DBHandler dbhandler = new DBHandler();
+
+		if (!dbhandler.connectToDatabase(config.getConfigData()))
+			System.exit(1);
+
+		// start server
+		Server server = new Server(config.getConfigData());
+
+		if (!server.startServer(dbhandler))
 			System.exit(1);
 	}
 }
