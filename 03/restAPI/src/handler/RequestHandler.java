f65d5d34e1eb1ace3681bd20cd5df9edbb3748cc
sd70 <saeed.haddadian@gmail.com>
2021-06-30 15:44:24 +0430

    RequestHandler class created
    this class handles http requests

---
+++ 03/restAPI/src/handler/RequestHandler.java
@@ -0,0 +1,12 @@
+package handler;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import cfg.Config;
+
+public class RequestHandler implements HttpHandler {
+	@Override
+	public void handle(HttpExchange ex){
+	}
+}
